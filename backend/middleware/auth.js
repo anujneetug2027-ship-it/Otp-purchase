d3e1adbf1297fd768ffa1be0ae10558b784e3c22
anujneetug2027-ship-it <anujneetug2027@gmail.com>
2025-12-29 21:37:38 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,7 @@
 export const isAuth = (req, res, next) => {
-  if (req.session.admin) next();
-  else res.status(401).json({ error: "Unauthorized" });
+  if (req.session.admin) {
+    next();
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
 };
