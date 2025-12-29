cc030b8c1cfdc4458283ab1976b4e2839053b8b4
anujneetug2027-ship-it <anujneetug2027@gmail.com>
2025-12-29 21:51:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
 
+console.log("🔥 server.js is being executed");
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server alive");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("✅ Listening on port", PORT);
+});
